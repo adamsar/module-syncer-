27ae5b555a23e657105bb83e1c0e18301516d842
Andrew Adams <andy@Andrews-MacBook-Pro.local>
2013-05-27 18:07:10 +0900

    better checking

--- fabfile.py
+++ fabfile.py
@@ -52,16 +52,23 @@ def restart_server(config, section):
     with cd(server):
         sudo("sh shutdown.sh")
         time.sleep(10)
-        if run("ps aux| grep %s | grep -v grep" % server):
-            sudo("ps aux | grep %s | grep -v grep | awk {'print $2'}| sudo xargs kill -s kill" % server)
+        with settings(warn_only=True):
+            if run("ps aux| grep %s | grep -v grep" % server):
+                sudo("ps aux | grep %s | grep -v grep | awk {'print $2'}| sudo xargs kill -s kill" % server)
 
         sudo("sh startup.sh")
         counts = 0
-        while(not run("ps aux | grep %s | grep -v grep" % server)):
-            print "Not found, sleeping and trying again in 10 seconds"
-            time.sleep(10)
-            sudo("sh startup.sh")
-            counts += 1
-            if counts >= 10:
-                print "Failed too many times, breaking out"
+        test = ""
+        while True:
+            with settings(warn_only=True):
+                test = run("ps aux | grep %s | grep -v grep" % server)
+            if not test:
+                print "Not found, sleeping and trying again in 10 seconds"
+                time.sleep(10)
+                sudo("sh startup.sh")
+                counts += 1
+                if counts >= 10:
+                    print "Failed too many times, breaking out"
+                    break
+            else:
                 break
