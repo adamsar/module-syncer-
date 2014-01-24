74230bc695fbd5c9ae1f009a8beb5ba347866a2c
adamsar <adamsar@gmail.com>
2014-01-24 11:48:55 +0900

    server change

--- fabfile.py
+++ fabfile.py
@@ -20,8 +20,8 @@ sudo rm script.cms
 env.roledefs ={
     'dev': ['localhost'],
     'stage': ['ec2-54-249-194-79.ap-northeast-1.compute.amazonaws.com'],
-    'devaoki': ['devaoki2.ubicast.com'],
-    'ikumi': ['54.248.215.164']
+    'devaoki': ['mymenuredhat.opencms.jp'],
+    'chef': ['ec2-54-199-33-32.ap-northeast-1.compute.amazonaws.com']
     }
 
 
